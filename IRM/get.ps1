96b66a8494ae448138a8438005f81ca4a0c01a16
Myitian <miaoyitian233@gmail.com>
2023-07-28 15:55:05 +0800

    ..

--- IRM/get.ps1
+++ IRM/get.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL1CN = 'https://cdn.jsdelivr.net/gh/Myitian/Microsoft-Activation-Scripts-CNTranslated@master/MAS/All-In-One-Version/MAS_AIO.cmd'
-$DownloadURL2CN = 'https://raw.githubusercontent.com/Myitian/Microsoft-Activation-Scripts-CNTranslated/master/MAS/All-In-One-Version/MAS_AIO.cmd'
+$DownloadURL1CN = 'https://cdn.jsdelivr.net/gh/Myitian/Microsoft-Activation-Scripts-CNTranslated@master/MAS/All-In-One-Version/MAS_AIO_GB2312.cmd'
+$DownloadURL2CN = 'https://raw.githubusercontent.com/Myitian/Microsoft-Activation-Scripts-CNTranslated/master/MAS/All-In-One-Version/MAS_AIO_GB2312.cmd'
 $DownloadURL1EN = 'https://gitlab.com/massgrave/microsoft-activation-scripts/-/raw/master/MAS/All-In-One-Version/MAS_AIO.cmd'
 $DownloadURL2EN = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO.cmd'
 
@@ -15,6 +15,7 @@ try {
     $response = Invoke-WebRequest -Uri $DownloadURL1CN -UseBasicParsing
 }
 catch {
+    "获取中文脚本失败，使用备用地址"
     try {
         $response = Invoke-WebRequest -Uri $DownloadURL2CN -UseBasicParsing
     }
@@ -24,6 +25,7 @@ catch {
             $response = Invoke-WebRequest -Uri $DownloadURL1EN -UseBasicParsing
         }
         catch {
+            "获取英文脚本失败，使用备用地址"
             $response = Invoke-WebRequest -Uri $DownloadURL2EN -UseBasicParsing
         }
     }
