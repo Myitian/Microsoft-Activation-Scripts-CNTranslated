1d934f0a550b40b9c6747d85bb472dc67e074d1d
Myitian <miaoyitian233@gmail.com>
2023-08-09 04:53:16 +0800

    awa

--- IRM/get_gb2312.ps1
+++ IRM/get_gb2312.ps1
@@ -2,8 +2,9 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL1CN = 'https://cdn.jsdelivr.net/gh/Myitian/Microsoft-Activation-Scripts-CNTranslated@master/MAS/All-In-One-Version/MAS_AIO_GB2312.cmd'
-$DownloadURL2CN = 'https://raw.githubusercontent.com/Myitian/Microsoft-Activation-Scripts-CNTranslated/master/MAS/All-In-One-Version/MAS_AIO_GB2312.cmd'
+$DownloadURL1CN = '' # 'https://gitlab.com/massgrave/microsoft-activation-scripts/-/raw/master/MAS/All-In-One-Version/MAS_AIO.cmd'
+$DownloadURL2CN = 'https://cdn.jsdelivr.net/gh/Myitian/Microsoft-Activation-Scripts-CNTranslated@master/MAS/All-In-One-Version/MAS_AIO_GB2312.cmd'
+$DownloadURL3CN = 'https://raw.githubusercontent.com/Myitian/Microsoft-Activation-Scripts-CNTranslated/master/MAS/All-In-One-Version/MAS_AIO_GB2312.cmd'
 $DownloadURL1EN = 'https://gitlab.com/massgrave/microsoft-activation-scripts/-/raw/master/MAS/All-In-One-Version/MAS_AIO.cmd'
 $DownloadURL2EN = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO.cmd'
 
@@ -15,22 +16,28 @@ try {
     $response = Invoke-WebRequest -Uri $DownloadURL1CN -UseBasicParsing
 }
 catch {
-    "获取中文脚本失败，使用备用地址"
+    "`u{83b7}`u{53d6}`u{4e2d}`u{6587}`u{811a}`u{672c}`u{5931}`u{8d25}`u{ff0c}`u{4f7f}`u{7528}`u{5907}`u{7528}`u{5730}`u{5740}"
     try {
         $response = Invoke-WebRequest -Uri $DownloadURL2CN -UseBasicParsing
     }
     catch {
-        "获取中文脚本失败，使用备用英文脚本"
+        "`u{83b7}`u{53d6}`u{4e2d}`u{6587}`u{811a}`u{672c}`u{5931}`u{8d25}`u{ff0c}`u{4f7f}`u{7528}`u{5907}`u{7528}`u{5730}`u{5740}"
         try {
-            $response = Invoke-WebRequest -Uri $DownloadURL1EN -UseBasicParsing
+            $response = Invoke-WebRequest -Uri $DownloadURL3CN -UseBasicParsing
         }
         catch {
+            "`u{83b7}`u{53d6}`u{4e2d}`u{6587}`u{811a}`u{672c}`u{5931}`u{8d25}`u{ff0c}`u{4f7f}`u{7528}`u{5907}`u{7528}`u{82f1}`u{6587}`u{811a}`u{672c}"
             try {
-                "获取英文脚本失败，使用备用地址"
-                $response = Invoke-WebRequest -Uri $DownloadURL2EN -UseBasicParsing
+                $response = Invoke-WebRequest -Uri $DownloadURL1EN -UseBasicParsing
             }
             catch {
-                "获取脚本失败"
+                try {
+                    "`u{83b7}`u{53d6}`u{82f1}`u{6587}`u{811a}`u{672c}`u{5931}`u{8d25}`u{ff0c}`u{4f7f}`u{7528}`u{5907}`u{7528}`u{5730}`u{5740}"
+                    $response = Invoke-WebRequest -Uri $DownloadURL2EN -UseBasicParsing
+                }
+                catch {
+                    "`u{83b7}`u{53d6}`u{811a}`u{672c}`u{5931}`u{8d25}"
+                }
             }
         }
     }
